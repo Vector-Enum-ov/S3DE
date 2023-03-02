6005fc72b6578dd926dd5a697abbd83987eac350
Igor Tkachev <igr.tkchv@gmail.com>
2023-03-02 22:28:13 +0300

    Added base of the project (glfw/glew and main.cpp file), configured CMakeList.

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+//
+// Created by VectorE on 01.03.2023.
+//
+
+#include <iostream>
+
+#define GLEW_STATIC
+#include "../include/glew.h"
+#include "../include/glfw3.h"
+
+int main(void) {
+    if (!glfwInit()) {
+        std::cout << "GLFW ERROR OCCURRED\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}

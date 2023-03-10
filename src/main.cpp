1d17c21545ade0b71f5cc9189c27ee55e533e667
Igor Tkachev <igr.tkchv@gmail.com>
2023-03-10 22:47:29 +0300

    Added shader class, that builds shader program.

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,22 @@
 #include <iostream>
 
 #define GLEW_STATIC
-#include "../include/glew.h"
-#include "../include/glfw3.h"
+#include "./../include/glew.h"
+#include "./../include/glfw3.h"
+
+#include "./shader/Shader.h"
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void switchFillPolygon();
+
+bool fillPolygon = true;
 
 int main(void) {
     if (!glfwInit()) {
         std::cout << "GLFW ERROR OCCURRED\n";
         return -1;
     }
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
     if (window == nullptr)
@@ -23,12 +31,40 @@ int main(void) {
     }
     glfwMakeContextCurrent(window);
 
+    glfwSetKeyCallback(window, key_callback);
+
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK)
     {
         std::cout << "Failed to initialize GLEW" << std::endl;
         return -1;
     }
+
+    glViewport(0, 0, 800, 600);
+
+    Shader shd = Shader("./../res/default.vertex", "./../res/default.fragment");
+
+    GLfloat vertices[] = {
+            // Positions                       // Colors
+            0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Right
+            -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Left
+            0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f   // Top
+    };
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
 
     while(!glfwWindowShouldClose(window))
     {
@@ -37,9 +73,32 @@ int main(void) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        shd.Use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
         glfwSwapBuffers(window);
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
     glfwTerminate();
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    else if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+        switchFillPolygon();
+}
+
+void switchFillPolygon() {
+    if (fillPolygon)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    else
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    fillPolygon = !fillPolygon;
 }
